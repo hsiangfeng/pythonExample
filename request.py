f98db9f30b4cceffc826407530d021d23e81e368
hsiangfeng <loveyou520babytw@gmail.com>
2021-04-11 22:17:06 +0800

    get remote blog data

---
+++ request.py
@@ -0,0 +1,16 @@
+import requests
+
+url = 'https://hsiangfeng.github.io/'
+response = requests.get(url)
+
+if response.status_code == 200:
+  fp = open('blog/index.html', 'w')
+  fp.write(response.text)
+  fp.close()
+  result = open('blog/index.html', 'r')
+  print(result.read())
+  result.close()
+else:
+  print('請求出現錯誤')
+
+
